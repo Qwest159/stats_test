e43a5193da5a0c18913b9c83301f628bd8200ac0
Qwest159 <duchesne-raphael@hotmail.com>
2024-09-23 14:09:23 +0200

    average

--- tests/Feature/AverageTest.php
+++ tests/Feature/AverageTest.php
@@ -10,9 +10,9 @@ test("peux calculer la moyenne d'une liste", function () {
 });
 
 //peut faire plusieur test comme ca (copier coller)
-test("calculer la moyenne d'une liste", function () {
-    $list = [10, 15, 14, 16, 20];
+// test("calculer la moyenne d'une liste", function () {
+//     $list = [10, 15, 14, 16, 20];
 
-    $result = Average::calculate($list);
-    expect($result)->ToBe(20.0);
-});
+//     $result = Average::calculate($list);
+//     expect($result)->ToBe(20.0);
+// });
